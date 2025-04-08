19f0c74f4e1e5d188a18fe6ce6f4a3c7cc75a1eb
Santic-eng <santicf32@gmail.com>
2025-04-08 23:21:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 
 // Beispielhafte Logik für spätere API-Integration
 console.log("GeoNewsTrader ready");
+// Beispiel-Update alle 60 Sekunden:
+setInterval(function() {
+  fetch('https://deine-news-api.com/latest')
+    .then(response => response.json())
+    .then(data => {
+      const newsDiv = document.getElementById('news');
+      const newsItem = document.createElement('p');
+      newsItem.innerHTML = `<strong>${data.time}</strong> – ${data.message} <br>➤ <span class="tip">${data.tip}</span>`;
+      newsDiv.prepend(newsItem); // Neue News stehen oben
+    });
+}, 60000); // Alle 60 Sekunden
